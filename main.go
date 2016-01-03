46f651fe1238625e189d8c591ff407f9f66e539b
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2016-01-03 15:15:16 +0000

    Add new command options. Refactor extract to function etc

--- main.go
+++ main.go
@@ -9,18 +9,37 @@ import (
 	"github.com/aws/aws-sdk-go/service/ec2"
 )
 
+const (
+	usageMsg = "Usage:\n\tmain region_name command"
+)
+
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage:\n\tmain region_name")
+	if len(os.Args) < 2 {
+		fmt.Println(usageMsg)
 		os.Exit(-1)
 	}
 
 	region := os.Args[1]
 	if region == "" {
-		fmt.Println("Usage:\n\tmain region_name")
+		fmt.Println(usageMsg)
 		os.Exit(-1)
 	}
 
+	command := os.Args[2]
+
+	switch command {
+	case "ls":
+		ls(region)
+		break
+	case "create":
+		fmt.Println("Under construction...")
+		break
+	default:
+		fmt.Println("Unknown command")
+	}
+}
+
+func ls(region string) {
 	svc := ec2.New(session.New(), &aws.Config{Region: aws.String(region)})
 
 	resp, err := svc.DescribeInstances(nil)
